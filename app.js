3c2e00cfb8fd354b1f29349a4ab35d119cfad494
tessxxx <akiijadhav22@gmail.com>
2018-11-23 20:19:06 +0530

    deleting some folders i missed

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const schema = require('./schema/schema'); // require schema file to use in midd
 const mongoose = require('mongoose'); // require mongoose to connect to mlab i.e. instance of mongodb online
 const cors = require('cors'); //allow query(front end which is on another server port 3000) send request to gql server on port 4000
 
-const server = express(); // initialize it so we can use methods
+const app = express(); // initialize it so we can use methods
 
 //allow cross-origin request i.e let client send request to server
-server.use(cors());
+app.use(cors());
 
 //connect to mmlab database
 //replace the db sting & pw with my own
@@ -17,10 +17,10 @@ mongoose.connection.once('open', () => {
 })
 
 //graphical representation of data and how it can be walked
-server.use('/graphql', graphqlHTTP({ //graphql middleware uses the supercharge endpoint route to communicate with express-graphql
+app.use('/graphql', graphqlHTTP({ //graphql middleware uses the supercharge endpoint route to communicate with express-graphql
   schema,
   graphiql: true
 }));
 
 const port = process.env.PORT || 9000;
-server.listen(port, () => console.log(`Listening on port ${port}`))
+app.listen(port, () => console.log(`Listening on port ${port}`))
